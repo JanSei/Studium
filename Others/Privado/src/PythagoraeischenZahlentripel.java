c6642d4f9ed4ae2dd1e908e261b1f5af89908d90
Jan Seidler <janseidler@192.168.30.49>
2014-07-07 22:48:16 +0200

    Paar aenderungen zu Pythagor

--- Others/Privado/src/PythagoraeischenZahlentripel.java
+++ Others/Privado/src/PythagoraeischenZahlentripel.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 
 
-public class PythagoräischenZahlentripel {
+public class PythagoraeischenZahlentripel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
