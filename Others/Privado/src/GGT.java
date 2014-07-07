14d1306ec21eb941ed59e0527e693100f25f3500
Jan Seidler <janseidler@192.168.30.49>
2014-07-07 22:29:03 +0200

    Und noch ein weiterer Update

--- Others/Privado/src/GGT.java
+++ Others/Privado/src/GGT.java
@@ -11,39 +11,37 @@ public class GGT {
 		y = Integer.parseInt(JOptionPane
 				.showInputDialog("Bitte zweite Zahl eingeben"));
 
-		while (x != y)
-			if (x > y)
-				x = x - y;
-			else
-				y = y - x;
+		// while (x != y)
+		// if (x > y)
+		// x = x - y;
+		// else
+		// y = y - x;
 
 		// Bessere Variante des ggT-Algorithmus
 		/*
-		 * while ((x > 0) && (y > 0)) 
-		 * 		if(x >= y) x = x % y; 
-		 * 		else y = y % x; 
-		 * z = x + y;
+		 * while ((x > 0) && (y > 0)) if(x >= y) x = x % y; else y = y % x; z =
+		 * x + y;
 		 */
 
 		// Rekursive Variante des ggT-Algorithmus
 		/*
-		 * int ggT(int x, int y) { 
-		 * 		if (x == y) return x; 
-		 * 		else if (x > y) return ggT(x-y, y); 
-		 * 		else return ggT(x, y-x); }
+		 * int ggT(int x, int y) { if (x == y) return x; else if (x > y) return
+		 * ggT(x-y, y); else return ggT(x, y-x); }
 		 */
 
-		// Rekursive Variante des ggT-Algorithmus aus den Internet geholt
-		/*
-		 * int ggt(int zahl1, int zahl2) { 
-		 * 		if (zahl1 >= zahl2) { 
-		 * 			if (zahl1 % zahl2 == 0) return zahl2; 
-		 * 		else return ggt(zahl2, zahl1 - zahl2); 
-		 * 		}
-		 * 		else return ggt(zahl2, zahl1); }
-		 */
+		JOptionPane.showMessageDialog(null, "Ergebnis ist: " + ggt(x, y));
+	}
 
-		JOptionPane.showMessageDialog(null, "Ergebnis ist: " + x);
+	// Rekursive Variante des ggT-Algorithmus aus den Internet geholt
+
+	static int ggt(int x, int y) {
+		if (x >= y) {
+			if (x % y == 0)
+				return y;
+			else
+				return ggt(y, x - y);
+		} else
+			return ggt(y, x);
 	}
 
 }
