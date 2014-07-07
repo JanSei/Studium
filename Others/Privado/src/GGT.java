71ca6377b81ff1debcd5e482f4419a1a6db26cf1
Jan Seidler <janseidler@192.168.30.49>
2014-07-07 22:24:11 +0200

    Ein update zum ggT

--- Others/Privado/src/GGT.java
+++ Others/Privado/src/GGT.java
@@ -1,35 +1,48 @@
 import javax.swing.*;
 
 public class GGT {
-	
+
 	public static void main(String[] args) {
-		
+
 		int x, y;
-		
-		x = Integer.parseInt(JOptionPane.showInputDialog("Bitte erste Zahl eingeben"));
-		y = Integer.parseInt(JOptionPane.showInputDialog("Bitte zweite Zahl eingeben"));
-		
+
+		x = Integer.parseInt(JOptionPane
+				.showInputDialog("Bitte erste Zahl eingeben"));
+		y = Integer.parseInt(JOptionPane
+				.showInputDialog("Bitte zweite Zahl eingeben"));
+
 		while (x != y)
-			if(x > y) x = x-y;
-			else y = y-x;
-		
+			if (x > y)
+				x = x - y;
+			else
+				y = y - x;
+
 		// Bessere Variante des ggT-Algorithmus
 		/*
-		 * while ((x > 0) && (y > 0))
-		 * 		if(x >= y) x = x % y;
-		 * 		else	   y = y % x;
+		 * while ((x > 0) && (y > 0)) 
+		 * 		if(x >= y) x = x % y; 
+		 * 		else y = y % x; 
 		 * z = x + y;
 		 */
-		
+
 		// Rekursive Variante des ggT-Algorithmus
 		/*
-		 * int ggT(int x, int y) {
-		 * 		if (x == y)  	return x;
-		 * 		else if (x > y)	return ggT(x-y, y);
-		 * 		else			return ggT(x, y-x);
-		 * }
+		 * int ggT(int x, int y) { 
+		 * 		if (x == y) return x; 
+		 * 		else if (x > y) return ggT(x-y, y); 
+		 * 		else return ggT(x, y-x); }
 		 */
-		
+
+		// Rekursive Variante des ggT-Algorithmus aus den Internet geholt
+		/*
+		 * int ggt(int zahl1, int zahl2) { 
+		 * 		if (zahl1 >= zahl2) { 
+		 * 			if (zahl1 % zahl2 == 0) return zahl2; 
+		 * 		else return ggt(zahl2, zahl1 - zahl2); 
+		 * 		}
+		 * 		else return ggt(zahl2, zahl1); }
+		 */
+
 		JOptionPane.showMessageDialog(null, "Ergebnis ist: " + x);
 	}
 
